a30e9c25ed5c42b141fe09e622651256366b382e
Andrew Ankrah <nanaegyir2003@gmail.com>
2015-11-04 22:50:06 -0500

    first commit

--- jquery.linkIt.js
+++ jquery.linkIt.js
@@ -1,1 +1,36 @@
-      dff
+/*
+ * name: LinkIt
+ * author: Andrew Ankrah
+ * version: 0.1.0
+ * license: MIT
+ */
+
+ (function($){
+ 	 $.fn.linkIt=function(options){
+	     //Default settings
+		 var settings = $.extend({
+		   href  : null,
+		   text  : null,
+		   target: '_self' //You could have also used '_blank'
+		 }, options);
+
+		 //validation 
+		 if(settings.href == null){
+		 	 console.log('You need an href option for LinkIt to work');
+			 return this;
+		 }
+
+		 return this.each(function(){
+		 	var object = $(this);
+
+			if(settings.text == null){
+			
+				 settings.text = object.text();
+			}
+			 object.wrap('<a target ="'+settings.target+'" href = "'+settings.href+'"></a>').text(settings.text);
+		 });
+
+		
+	 };																												 
+ 
+ }(jQuery));
